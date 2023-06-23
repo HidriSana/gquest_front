1a31de31ae80f99e5089a59e4b2433557a969f23
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2023-06-23 13:43:42 +0200

    Changing the behaviour of the ascii  section: script in index.html instead of onload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,58 +6,6 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     
-    const handleEvent = () => {
-
-        console.log(`
-█▀ ▀█▀ █▀█ █▀▀ ▀█▀ █▀▀ █░█
-▄█ ░█░ █▀▄ ██▄ ░█░ █▄▄ █▀█
-        `)
-        console.log(`
-█▀▀ █▀█ █▄░█ █▀ █▀█ █░░ █▀▀
-█▄▄ █▄█ █░▀█ ▄█ █▄█ █▄▄ ██▄
-        `)
-        console.log(`
-█▄█ █▀█ █░█  █░█ █▀▀   █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █░█ █ █▀   █▀▀ ▄▀█ █▀█
-░█░ █▄█ █▄█  ▀▄▀ ██▄   █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▀█ █ ▄█   █▀░ █▀█ █▀▄⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                                       
-        `);
-        console.log(`
-▀█▀ █▀█   █▀ █▀▀ █▀▀   █▀▄▀█ █▄█   █░█░█ █▀█ █▀█ █▄▀
-░█░ █▄█   ▄█ ██▄ ██▄   █░▀░█ ░█░   ▀▄▀▄▀ █▄█ █▀▄ █░█
-        `)
-        console.log(`
-▀█▀ █░█ ▄▀█ █▄░█ █▄▀   █▄█ █▀█ █░█
-░█░ █▀█ █▀█ █░▀█ █░█   ░█░ █▄█ █▄█
-        `)
-        console.log(`
-        ⢀⣠⣤⣤⣶⣶⣶⣶⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⣤⣾⣿⣿⠿⠟⠛⠛⠛⠛⠻⠿⣿⣿⣷⣤⡀⠀⠀⠀⠀
-⠀⠀⠀⣴⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣦⠀⠀⠀
-⠀⢀⣾⣿⡿⠁⠀⠀⣴⣦⣄⠀⠀⠀⠀⠀⣀⣤⣶⡀⠈⢿⣿⣷⡀⠀
-⠀⣾⣿⡟⠁⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠈⢻⣿⣷⠀
-⢠⣿⣿⠁⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠈⣿⣿⡄
-⢸⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⣿⣿⡇   https://github.com/HidriSana/gquest_front
-⠘⣿⣿⡦⠤⠒⠒⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠧⠤⢴⣿⣿⠃
-⠀⢿⣿⣧⡀⠀⢤⡀⠙⠻⠿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⢀⣼⣿⡿⠀
-⠀⠈⢿⣿⣷⡀⠈⢿⣦⣤⣾⣿⣿⣿⣿⣿⣷⣄⠀⠀⢀⣾⣿⡿⠁⠀
-⠀⠀⠀⠻⣿⣿⣦⣄⡉⣿⣿⢿⣿⠉⢻⣿⢿⣿⣠⣴⣿⣿⠟⠀⠀⠀
-⠀⠀⠀⠀⠈⠛⢿⣿⣿⣿⣧⣼⣿⣤⣾⣷⣶⣿⣿⡿⠛⠁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⠿⠿⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀
-        `)
-        console.log(`
-▄▀█ █▄░█ █▀▄   █▀█ █▀▀ █▀▄▀█ █▀▀ █▀▄▀█ █▄▄ █▀▀ █▀█
-█▀█ █░▀█ █▄▀   █▀▄ ██▄ █░▀░█ ██▄ █░▀░█ █▄█ ██▄ █▀▄
-        `);
-        console.log(`
-█ █▄░█ █░░ █ █▄░█ █▀▀ █▄▄ █░░ █▀█ █▀▀ █▄▀
-█ █░▀█ █▄▄ █ █░▀█ ██▄ █▄█ █▄▄ █▄█ █▄▄ █░█
-                                                          
-        `)
-        console.log(`
-█ █▀   █░█ █▄░█ █▀▄ █▀▀ █▀█ █▀█ ▄▀█ ▀█▀ █▀▀ █▀▄
-█ ▄█   █▄█ █░▀█ █▄▀ ██▄ █▀▄ █▀▄ █▀█ ░█░ ██▄ █▄▀                                                                              
-        `)
-      };
-    
     return (
         <section className="home">
             <h1  className="main-title">GQUEST</h1> 
@@ -66,7 +14,7 @@ const Home = () => {
                 Rejoins ta guilde, complète des quêtes, montre ta valeur et transforme tes points d'expérience en récompense
             </h2>
             <div className = "hero">
-                <img onLoad={handleEvent} className='card' src="/svg/exclamation-mark-svgrepo-com.svg" alt="Exclamation mark for pending quest"/>
+                <img className='card' src="/svg/exclamation-mark-svgrepo-com.svg" alt="Exclamation mark for pending quest"/>
                 <img className='card' src="/svg/question-mark-svgrepo-com.svg" alt="Question mark for completing quest"/>
                 <img className='card' src="/svg/favorites-svgrepo-com.svg" alt="Star  for experience after completing a quest"/>
                 <img className='card' src="/svg/gift-svgrepo-com.svg" alt="Gift for redeeming experience points"/>
